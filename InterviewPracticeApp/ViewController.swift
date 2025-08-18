2655bb8a00035aa4ae16fc115648dba03e28f6c0
EmerBV <emersmc@hotmail.com>
2025-08-18 14:38:37 +0200

    Initial Commit

---
+++ InterviewPracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewPracticeApp
+//
+//  Created by Emerson Balahan Varona on 18/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
